1648651d7ba5031dd1bac72eac9fffd39b653759
Shmateo-jr <96316504+Shmateo-jr@users.noreply.github.com>
2025-07-25 00:32:20 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Brian Kuria",
+  botOwner: 'Btian Kuria', // Your name
+  ownerNumber: '254772469043', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
